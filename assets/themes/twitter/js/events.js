7f54c09d34c5b4306112c1322f4cd5e7951614cb
Kin Lane <kinlane@gmail.com>
2012-11-21 21:44:32 -0800

    update

--- assets/themes/twitter/js/events.js
+++ assets/themes/twitter/js/events.js
@@ -3,7 +3,18 @@ function showEvents()
     alert("firing!");	
     $.getJSON('data/events/san-francisco.json', function(data) {
 	alert('in');
-       
+        $.each(data['events'], function(key, val) {
+        	alert(val['start_date']);
+			if ( new Date(val['start_date']) > new Date() )
+			{
+					
+	        var template = $('#eventListingTemplate').html();
+	        var html = Mustache.to_html(template, data);
+	        //alert(html);
+	        $('#eventList').html(html);
+	                          
+            }
+          }); 
         });
     }    
      
