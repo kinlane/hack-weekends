8364a37fd4f992519cc332d258021a03e95ecad4
Kin Lane <kinlane@gmail.com>
2012-11-21 22:29:18 -0800

    update

--- assets/themes/twitter/js/events.js
+++ assets/themes/twitter/js/events.js
@@ -5,7 +5,6 @@ function showEvents()
 	//alert('in');
         $.each(data['events'], function(key, val) {
         	startDate = val['start_date'] + " 00:00:00 AM";
-        	startDate = startDate.toDateString();
         	startDate = new Date(startDate);
         	today = new Date();
         	alert(startDate + ' < ' + today);
