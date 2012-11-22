5f455a02315e77d2a80b87f0b8ff9bef0c6eadbe
Kin Lane <kinlane@gmail.com>
2012-11-21 23:50:13 -0800

    update

--- assets/themes/twitter/js/events.js
+++ assets/themes/twitter/js/events.js
@@ -8,7 +8,8 @@ function showEvents()
         	
         	startDate = val['start_date'];
         	startDateArray = startDate.split('-',3);
-        	alert(startDateArray[0]);
+        	startDate = new Date(startDateArray[2], startDateArray[0], startDateArray[1]);
+        	alert(startDate);
 			
           }); 
         });
