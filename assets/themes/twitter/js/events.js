49fcc9e354ee746f2fee39d7514807097ad984fb
Kin Lane <kinlane@gmail.com>
2012-11-23 09:42:59 -0800

    update

--- assets/themes/twitter/js/events.js
+++ assets/themes/twitter/js/events.js
@@ -21,7 +21,7 @@ function showEvents()
 function showEventsByCity()
     {
     eventcount = 0;	
-    allEvents = new Array();
+    allEvents = new Array({"name":"name", "start_date": "", "display_start_date": "", "city": "", "country": ""});
 	
     $.getJSON('data/events/cities.json', function(data) {
     	
@@ -53,7 +53,7 @@ function showEventsByCity()
 					        var thisEvent = { "name":val2['name'], "start_date": val2['start_date'], "display_start_date": val2['display_start_date'], "city": val2['city'], "country": val2['country']};
 					        
 							//alert(dodump(thisEvent));
-					        var doit = allEvents.unshift({ "name":val2['name'], "start_date": val2['start_date'], "display_start_date": val2['display_start_date'], "city": val2['city'], "country": val2['country']});	
+					        var doit = allEvents.push({ "name":val2['name'], "start_date": val2['start_date'], "display_start_date": val2['display_start_date'], "city": val2['city'], "country": val2['country']});	
 					        eventcount++;  	
 					        alert(dodump(doit));
 			        		}
@@ -64,7 +64,7 @@ function showEventsByCity()
 	        	}
           });     
         }); 
-     alert(dodump(allEvents),6);  
+     alert(dodump(allEvents[0]));  
     }       
      
      
