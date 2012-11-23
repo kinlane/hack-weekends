63d05f85b5f0dd7bc1b5a17e7ca5b0fdb1881a67
Kin Lane <kinlane@gmail.com>
2012-11-23 09:40:07 -0800

    update

--- assets/themes/twitter/js/events.js
+++ assets/themes/twitter/js/events.js
@@ -53,7 +53,7 @@ function showEventsByCity()
 					        var thisEvent = { "name":val2['name'], "start_date": val2['start_date'], "display_start_date": val2['display_start_date'], "city": val2['city'], "country": val2['country']};
 					        
 							//alert(dodump(thisEvent));
-					        var doit = allEvents.push({ "name":val2['name'], "start_date": val2['start_date'], "display_start_date": val2['display_start_date'], "city": val2['city'], "country": val2['country']});	
+					        var doit = allEvents.unshift({ "name":val2['name'], "start_date": val2['start_date'], "display_start_date": val2['display_start_date'], "city": val2['city'], "country": val2['country']});	
 					        eventcount++;  	
 					        alert(dodump(doit));
 			        		}
@@ -64,7 +64,7 @@ function showEventsByCity()
 	        	}
           });     
         }); 
-     //alert(dodump(allEvents[0]));  
+     alert(dodump(allEvents),6);  
     }       
      
      
