9b37934bdbd06bd7207d21ee8c75cec223eb5f1b
Kin Lane <kinlane@gmail.com>
2012-11-23 09:50:10 -0800

    update

--- assets/themes/twitter/js/events.js
+++ assets/themes/twitter/js/events.js
@@ -46,13 +46,10 @@ function showEventsByCity()
 			        	if(startDate > endDate)
 			        		{
 			        	
-					        var template = $('#eventListingItemTemplate').html();
-					        var html = Mustache.to_html(template, val2);
-					        $('#eventList').append(html);
+					        //var template = $('#eventListingItemTemplate').html();
+					        //var html = Mustache.to_html(template, val2);
+					        //$('#eventList').append(html);
 					        
-					        var thisEvent = { "name":val2['name'], "start_date": val2['start_date'], "display_start_date": val2['display_start_date'], "city": val2['city'], "country": val2['country']};
-					        
-							//alert(dodump(thisEvent));
 					        var doit = allEvents.push({ "name":val2['name'], "start_date": val2['start_date'], "display_start_date": val2['display_start_date'], "city": val2['city'], "country": val2['country']});	
 					        eventcount++;  	
 					        alert(dodump(doit));
