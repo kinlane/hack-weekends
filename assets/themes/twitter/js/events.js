aebacd27e63aa175cad6f51e203d18f6dc64ce25
Kin Lane <kinlane@gmail.com>
2012-11-24 11:54:10 -0800

    update

--- assets/themes/twitter/js/events.js
+++ assets/themes/twitter/js/events.js
@@ -96,7 +96,7 @@ function loadTableSort()
 	{
 	alert("turning on - 11");
 	//$("#eventList").tablesorter(); 
-	$("#eventList").tablesorter( {sortList: [[0,0], [1,0]]} ); 
+	//$("#eventList").tablesorter( {sortList: [[0,0], [1,0]]} ); 
 	alert("turning on - 22");
 	}
     
