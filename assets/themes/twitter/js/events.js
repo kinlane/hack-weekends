74d4b129d17556784ac6128698c7b794b1a82769
Kin Lane <kinlane@gmail.com>
2012-11-24 13:44:33 -0800

    update

--- assets/themes/twitter/js/events.js
+++ assets/themes/twitter/js/events.js
@@ -20,16 +20,9 @@ function showEvents()
     
 function callback(json){
 	alert("IN");
-    $.each(json['events'], function(key2, val2) {
+    $.each(json, function(key, val) {
     	  
-    	startDate = new Date(val2['start_date']);
-    	endDate = new Date();
-
-    	if(startDate > endDate)
-    		{
-	        aEvent = { "name":val2['name'], "start_date": val2['start_date'], "display_start_date": val2['display_start_date'], "city": val2['city'], "country": val2['country']};
-	        var doit = allEvents.push(aEvent);	
-    		}
+    	alert(val);
       }); 
 	}     
     
