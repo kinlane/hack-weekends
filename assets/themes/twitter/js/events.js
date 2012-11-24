7d5c0f816dfdca4ef85cb340089fbaa923f97e6b
Kin Lane <kinlane@gmail.com>
2012-11-24 15:09:41 -0800

    update

--- assets/themes/twitter/js/events.js
+++ assets/themes/twitter/js/events.js
@@ -27,6 +27,7 @@ function addDisplayEvent(eventdata){
     		{
 	        aEvent = { "name":val2['name'], "start_date": val2['start_date'], "display_start_date": val2['display_start_date'], "city": val2['city'], "country": val2['country']};
 	        var doit = allEvents.push(aEvent);	
+	        alert(doit);
     		}
     	 });
 	}    
@@ -35,7 +36,7 @@ function doDisplayEvent(currCount,totalCount)
 {
 	if(currCount==totalCount){
 	alert(currCount+':'+totalCount);
-	$.each(allEvents, function(key3, val3) { alert(val3); });
+	//$.each(allEvents, function(key3, val3) { alert(val3); });
 	}
 		
 } 
