9e84a1b36bc566c14f01ca98bc99bf9ba6743ac0
Kin Lane <kinlane@gmail.com>
2012-11-24 10:21:26 -0800

    update

--- assets/themes/twitter/js/events.js
+++ assets/themes/twitter/js/events.js
@@ -46,56 +46,16 @@ function showEventsByCity()
 
 			        	if(startDate > endDate)
 			        		{
-					        aEvent = { "name":val2['name'], "start_date": val2['start_date'], "display_start_date": val2['display_start_date'], "city": val2['city'], "country": val2['country']};
-					        var doit = allEvents.push(aEvent);	
+					        var template = $('#eventListingItemTemplate').html();
+					        var html = Mustache.to_html(template, val2);
+					        //alert(html);
+					        $('#eventList').append(html);  
 			        		}
 			          }); 
 			          
 			        });
 			               		        	
-	        	}
-	        	
-          		citycount++; 
-	          	if(totalcities==citycount){
-	          	       	
-          		alert(totalcities + ":" + citycount);
-			          	
-				var aTemp = [];
-			    for (var sKey in allEvents){
-			        aTemp[aTemp.length] = sKey; 
-			    }
-			    
-			    aTemp.sort(function(a,b){
-			    	
-			    	//alert(allEvents[aTemp[a]].start_date);
-			    	//alert(allEvents[aTemp[b]].start_date);
-			    	
-				    if(allEvents[aTemp[a]].start_date < allEvents[aTemp[b]].start_date) return -1;
-				    if(allEvents[aTemp[a]].start_date > allEvents[aTemp[b]].start_date) return 1;
-				    
-				    //if(allEvents[aTemp[a]].name < allEvents[aTemp[b]].name) return -1;
-				    //if(allEvents[aTemp[a]].name > allEvents[aTemp[b]].name) return 1;				    
-				    
-				    return 0;
-				});
-							    
-				 var aOutput = {};
-				    for (var nIndex=0; nIndex<aTemp.length;nIndex++){
-				        aOutput[aTemp[nIndex]] = allEvents[aTemp[nIndex]];
-				    }			    
-			              	
-  	
-          		$.each(aOutput, function(key3, val3) {
-			        var template = $('#eventListingItemTemplate').html();
-			        var html = Mustache.to_html(template, val3);
-			        //alert(html);
-			        $('#eventList').append(html);  
-			               			
-          			}); 
-          			
-          		//alert('done!');	
-          			
-	          	}   	        	       	
+	        	}	        	       	
           });           
             
         }); 
