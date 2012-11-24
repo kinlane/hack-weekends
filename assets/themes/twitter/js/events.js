3c900efba9152625c3acca158588b46b1d78ff27
Kin Lane <kinlane@gmail.com>
2012-11-24 09:50:34 -0800

    update

--- assets/themes/twitter/js/events.js
+++ assets/themes/twitter/js/events.js
@@ -87,12 +87,12 @@ function showEventsByCity()
           		$.each(aOutput, function(key3, val3) {
 			        var template = $('#eventListingItemTemplate').html();
 			        var html = Mustache.to_html(template, val3);
-			       // alert(html);
+			        alert(html);
 			        $('#eventList').append(html);  
 			               			
           			}); 
           			
-          		alert('done!');	
+          		//alert('done!');	
           			
 	          	}   	        	       	
           });           
