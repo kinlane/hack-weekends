fc115fd252a1771fea216e8008c242fc928e50f0
Kin Lane <kinlane@gmail.com>
2012-11-23 18:54:55 -0800

    update

--- assets/themes/twitter/js/events.js
+++ assets/themes/twitter/js/events.js
@@ -53,7 +53,7 @@ function showEventsByCity()
 					        //$('#eventList').append(html);
 					       //alert(dodump(val2,5));
 					        aEvent = { "name":val2['name'], "start_date": val2['start_date'], "display_start_date": val2['display_start_date'], "city": val2['city'], "country": val2['country']};
-					        alert(dodump(aEvent));
+					        alert("1:"+dodump(aEvent));
 					        var doit = allEvents.push(aEvent);	
 			        		}
 			          }); 
@@ -68,7 +68,8 @@ function showEventsByCity()
 	          		alert(totalcities + ":" + citycount);
 	          		
 	          		$.each(allEvents, function(key, val) {
-	          			alert(val['name']);
+	          			//alert(val['name']);
+	          			alert("2:"+dodump(val));
 	          			
 	          			//jEvent = JSON.stringify(val);
 	          			//alert(jEvent);
