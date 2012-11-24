da2f4a0c0870e55bbc835351a098cb8f8df022c8
Kin Lane <kinlane@gmail.com>
2012-11-24 10:23:07 -0800

    update

--- assets/themes/twitter/js/tablesorter.js
+++ assets/themes/twitter/js/tablesorter.js
@@ -1,20 +1,3 @@
-/*
-  SortTable
-  version 2
-  7th April 2007
-  Stuart Langridge, http://www.kryogenix.org/code/browser/sorttable/
-
-  Instructions:
-  Download this file
-  Add <script src="sorttable.js"></script> to your HTML
-  Add class="sortable" to any table you'd like to make sortable
-  Click on the headers to sort
-
-  Thanks to many, many people for contributions and suggestions.
-  Licenced as X11: http://www.kryogenix.org/code/browser/licence.html
-  This basically means: do what you want with it.
-*/
-
 
 var stIsIE = /*@cc_on!@*/false;
 
