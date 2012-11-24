40680f6b7ade6d96957790d83e3200dbfd8470b8
Kin Lane <kinlane@gmail.com>
2012-11-24 10:15:27 -0800

    update

--- assets/themes/twitter/js/events.js
+++ assets/themes/twitter/js/events.js
@@ -1,3 +1,4 @@
+var allEvents = new Array();
 
 function showEvents()
     {
@@ -21,7 +22,6 @@ function showEventsByCity()
     {
     citycount = 0;
     eventcount = 0;	
-    var allEvents = new Array();
 	
     $.getJSON('data/events/cities.json', function(data) {
     	allcities = data['cities'];
@@ -53,49 +53,8 @@ function showEventsByCity()
 			          
 			        });
 			               		        	
-	        	}
-	        	
-          		citycount++; 
-	          	if(totalcities==citycount){
-	          	       	
-          		alert(totalcities + ":" + citycount);
-			          	
-				var aTemp = [];
-			    for (var sKey in allEvents){
-			        aTemp[aTemp.length] = sKey; 
-			    }
-			    
-			    aTemp.sort(function(a,b){
-			    	
-			    	//alert(allEvents[aTemp[a]].start_date);
-			    	//alert(allEvents[aTemp[b]].start_date);
-			    	
-				    if(allEvents[aTemp[a]].start_date < allEvents[aTemp[b]].start_date) return -1;
-				    if(allEvents[aTemp[a]].start_date > allEvents[aTemp[b]].start_date) return 1;
-				    
-				    //if(allEvents[aTemp[a]].name < allEvents[aTemp[b]].name) return -1;
-				    //if(allEvents[aTemp[a]].name > allEvents[aTemp[b]].name) return 1;				    
-				    
-				    return 0;
-				});
-							    
-				 var aOutput = {};
-				    for (var nIndex=0; nIndex<aTemp.length;nIndex++){
-				        aOutput[aTemp[nIndex]] = allEvents[aTemp[nIndex]];
-				    }			    
-			              	
-  	
-          		$.each(aOutput, function(key3, val3) {
-			        var template = $('#eventListingItemTemplate').html();
-			        var html = Mustache.to_html(template, val3);
-			        //alert(html);
-			        $('#eventList').append(html);  
-			               			
-          			}); 
-          			
-          		//alert('done!');	
-          			
-	          	}   	        	       	
+	        	}        	
+          		citycount++;   	        	       	
           });           
             
         }); 
