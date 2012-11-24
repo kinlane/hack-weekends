f20ab50ca33027b2be0b96b12bb0322e99e7761e
Kin Lane <kinlane@gmail.com>
2012-11-23 20:47:10 -0800

    update

--- assets/themes/twitter/js/events.js
+++ assets/themes/twitter/js/events.js
@@ -87,10 +87,7 @@ function showEventsByCity()
 			    for(var key in allEvents) {
 			        temp.push(allEvents[key].name);
 			    }
-			    temp = temp.sort();
-			    for(var x = 0; x < temp.length; x++) {
-			        results[x] = allEvents[temp[x]];
-			    }	          	
+          	
 	          	
           		alert(totalcities + ":" + citycount);
           		//alert(allEvents.length);
