fadd56aeb2db242f61cacd4ac6962cb4dd9fb4eb
Kin Lane <kinlane@gmail.com>
2012-11-25 00:59:33 -0800

    update

--- assets/themes/twitter/js/events.js
+++ assets/themes/twitter/js/events.js
@@ -16,15 +16,18 @@ function addDisplayEvent(eventdata){
 	
 function doDisplayEvent(currCount,totalCount)
 {
-	if(currCount==totalCount)
-	{
+	if(currCount==totalCount){
 	alert(currCount+':'+totalCount);
 	alert(allEvents[0]['name']);
 	alert(allEvents[1]['name']);
 	alert(allEvents[2]['name']);
-
 	
-	alert("done111");
+	var aTemp = [];
+    for (var sKey in allEvents){
+        aTemp[aTemp.length] = sKey; 
+    }
+    
+	alert("done22");
 
 	}	
 } 
