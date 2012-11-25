5c42e1580067e2452d05cf0b1c80a3e1e5c253e7
Kin Lane <kinlane@gmail.com>
2012-11-25 01:08:54 -0800

    update

--- assets/themes/twitter/js/events.js
+++ assets/themes/twitter/js/events.js
@@ -22,13 +22,7 @@ function doDisplayEvent(currCount,totalCount)
 	alert(allEvents[1]['name']);
 	alert(allEvents[2]['name']);
 	
-	var aTemp = [];
-    for (var sKey in allEvents){
-        alert(sKey);
-    }
-
-	
-	alert("done11");
+	alert("done22");
 
 	}	
 } 
