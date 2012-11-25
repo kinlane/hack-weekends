d8e679fe877d1217e6d5c15b1465f28c4049c9b7
Kin Lane <kinlane@gmail.com>
2012-11-24 19:05:22 -0800

    update

--- assets/themes/twitter/js/events.js
+++ assets/themes/twitter/js/events.js
@@ -30,7 +30,7 @@ function doDisplayEvent(currCount,totalCount)
     aTemp.sort(function(a,b){
     	
     	///alert(aTemp[a]);
-    	alert(allEvents[aTemp[a]]['start_date']);
+    	alert(dodump(allEvents[aTemp[a]]));
     	
 	    //if(allEvents[aTemp[a]]['start_date'] < allEvents[aTemp[b]]['start_date']) return -1;
 	    //if(allEvents[aTemp[a]]['start_date'] > allEvents[aTemp[b]]['start_date']) return 1;
