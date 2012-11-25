2570cbe76193189ae43f7881f217223aa35a1db5
Kin Lane <kinlane@gmail.com>
2012-11-24 20:25:53 -0800

    update

--- assets/themes/twitter/js/events.js
+++ assets/themes/twitter/js/events.js
@@ -18,31 +18,11 @@ function doDisplayEvent(currCount,totalCount)
 {
 	if(currCount==totalCount){
 	alert(currCount+':'+totalCount);
-	//alert(allEvents[0]['name']);
+	alert(allEvents[0]['name']);
 	//alert(allEvents[1]['name']);
 	//alert(allEvents[2]['name']);
 	
-	var aTemp = [];
-    for (var sKey in allEvents){
-        aTemp[aTemp.length] = sKey; 
-    }
-    
-    aTemp.sort(function(a,b){
-    	
-    	//alert(dodump(allEvents[aTemp[a]]));
-    	//item1 = allEvents[aTemp[a]];
-    	
-    	//alert(dodump(item1));
-    	
-	    //if(allEvents[aTemp[a]]['start_date'] < allEvents[aTemp[b]]['start_date']) return -1;
-	    //if(allEvents[aTemp[a]]['start_date'] > allEvents[aTemp[b]]['start_date']) return 1;
-	    
-	    //if(allEvents[aTemp[a]].name < allEvents[aTemp[b]].name) return -1;
-	    //if(allEvents[aTemp[a]].name > allEvents[aTemp[b]].name) return 1;				    
-	    
-	    return 1;
-	});
-	
+
 	alert("done2");
 
 	}	
