979eb0832d6808dcf7987db4c78c62686fdd5615
Kin Lane <kinlane@gmail.com>
2012-11-30 20:55:46 -0800

    update

--- assets/themes/twitter/js/events.js
+++ assets/themes/twitter/js/events.js
@@ -70,10 +70,10 @@ function getEvent(file,id)
     {
     $.getJSON('data/events/'+file, function(data) {
         $.each(data['events'], function(key, val) {
-            if(id==val['id']){
-                var template = $('#directoryDetailTemplate').html();
+            if(url==val['url']){
+                var template = $('#eventDetailTemplate').html();
                 var html = Mustache.to_html(template, val);
-                $('#DirectoryDetail').html(html);                            
+                $('#EventDetail').html(html);                            
             }
           });                            
         });
