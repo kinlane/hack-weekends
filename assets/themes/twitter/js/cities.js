4935b6cce348bfb8aeab2ba9b159fafa9bf0eb59
Kin Lane <kinlane@gmail.com>
2012-12-07 19:36:55 -0800

    update

--- assets/themes/twitter/js/cities.js
+++ assets/themes/twitter/js/cities.js
@@ -50,7 +50,7 @@ function getCityEvents(city)
         $.each(data['events'], function(key, val) {
         	startDate = new Date(val['start_date']);
         	endDate = new Date();
-alert(val['name']+ ' ' + startDate + ' ' + endDate);
+			alert(val['name']+ ' ' + startDate + ' ' + endDate);
         	if(startDate > endDate)
         		{
                 var template = $('#eventListingItemTemplate').html();
