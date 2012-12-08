9a36d2a1186c3d59fc63dfc7f4390c7c5e3d6b93
Kin Lane <kinlane@gmail.com>
2012-12-07 22:08:52 -0800

    Limit news to 250

--- assets/themes/twitter/js/news.js
+++ assets/themes/twitter/js/news.js
@@ -1,13 +1,17 @@
 function listNews()
     {
+    newscount = 0;
     $.getJSON('data/news/news.json', function(data) {
     	
     	 $.each(data['news'], function(key, val) {
 	    	
-	        var template = $('#newsListingTemplate').html();
-	        var html = Mustache.to_html(template, val);
-	        $('#newsList').append(html);
+			if(newscount<250){
+				var template = $('#newsListingTemplate').html();
+				var html = Mustache.to_html(template, val);
+				$('#newsList').append(html);
+				}
 	        
+	        newscount++;
 	        });
         
         });
