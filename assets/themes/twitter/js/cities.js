773ac015c28cefb095b8aef71eef40018fa161d5
Kin Lane <kinlane@gmail.com>
2012-12-07 16:15:14 -0800

    Update cities section

---
+++ assets/themes/twitter/js/cities.js
@@ -0,0 +1,29 @@
+function showCities()
+    {
+    citycount = 0;
+    eventcount = 0;	
+    var allEvents = new Array();
+	
+    $.getJSON('data/events/cities.json', function(data) {
+    	
+    	allcities = data['cities'];
+    	totalcities = allcities.length;
+    	
+        $.each(data['cities'], function(key, val) {
+        	
+        	city = val['city'];
+        	     	
+        	if(val['eventcount']>1)
+	        	{
+		        var template = $('#cityListingItemTemplate').html();
+		        var html = Mustache.to_html(template, val2);
+		        $('#cityList').append(html);     
+		        citycount++;		        	
+	        	}	        	       	
+          });           
+            
+        }); 
+ 
+    }       
+     
+     
