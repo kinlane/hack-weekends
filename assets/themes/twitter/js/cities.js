8d2fd68bcc653bb275f530f9254400cde6b199fd
Kin Lane <kinlane@gmail.com>
2012-12-08 08:26:26 -0800

    update city json link

--- assets/themes/twitter/js/cities.js
+++ assets/themes/twitter/js/cities.js
@@ -48,6 +48,7 @@ function getCityEvents(city)
 	filename = 'data/events/'+ filename + ".json";    	
    	eventcount = 0;
    	
+   	alert(githubJSONFile);
 	$("jsonlink").attr("href", githubJSONFile)
 
    	$('#eventListTitle').html('Hackathons for ' + city);
