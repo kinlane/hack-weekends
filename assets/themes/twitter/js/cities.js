daab7a68779845b4728dcae855b3f8c56101dbfa
Kin Lane <kinlane@gmail.com>
2012-12-07 19:12:15 -0800

    calibrating columns

--- assets/themes/twitter/js/cities.js
+++ assets/themes/twitter/js/cities.js
@@ -20,14 +20,14 @@ function showCities()
 		        $('#tier1').append(html);     
 		        citycount++;		        	
 	        	}	
-        	if(val['eventcount']>5&&val['eventcount']<10)
+        	if(val['eventcount']>=5&&val['eventcount']<=10)
 	        	{
 		        var template = $('#cityListingItemTemplate').html();
 		        var html = Mustache.to_html(template, val);
 		        $('#tier2').append(html);     
 		        citycount++;		        	
 	        	}
-        	if(val['eventcount']<5)
+        	if(val['eventcount']<=5)
 	        	{
 		        var template = $('#cityListingItemTemplate').html();
 		        var html = Mustache.to_html(template, val);
