6f1d72a2fc4389eab4ff00921e73ddffc84e006d
Kin Lane <kinlane@gmail.com>
2012-12-07 16:32:37 -0800

    Loop through each city

--- assets/themes/twitter/js/cities.js
+++ assets/themes/twitter/js/cities.js
@@ -24,6 +24,13 @@ function showCities()
           });           
             
         }); 
+        
+        
+    $usCities = $('cloudUS').html();
+	$('li').each(function($usCities) {
+	    alert(index + ': ' + $(this).text());
+	});    
+    
  
     }       
      
